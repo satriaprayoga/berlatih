5e454216ec232254ef588bb03da751b3451a61d0
satriaprayoga <satria.prayoga@gmail.com>
2012-07-17 12:06:27 +0700

    mock login

---
+++ controllers/login.php
@@ -0,0 +1,17 @@
+<?php
+    class Login extends Controller{
+    	
+		public function __construct(){
+			
+		}
+    	
+		public function index(){
+			echo 'Login here';
+		}
+		
+		public function auth(){
+			
+		}
+		
+		
+    }
